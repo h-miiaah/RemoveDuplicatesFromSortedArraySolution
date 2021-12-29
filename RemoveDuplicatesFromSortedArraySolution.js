b5bef8f75ae8d807577d1ac4237bf1eaef19bd9e
h-miiaah <haim.miiaah5@gmail.com>
2021-12-28 22:14:07 -0500

    added comments explaining the solution

--- RemoveDuplicatesFromSortedArraySolution.js
+++ RemoveDuplicatesFromSortedArraySolution.js
@@ -18,5 +18,9 @@
 
 - set length of the updated array to 0
 - loop through all of the elements in the array
+- check if the current element is equal to the next element, if it is we skip it and continue to the next element for as long as we encounter a duplicate element
+- if the elements are not duplicates, then we place the different element next to the current element we are on
+- we update the array in place
+- return the array without the duplicates
 
 */
